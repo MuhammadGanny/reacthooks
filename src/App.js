3e7286326bca8eadaf6d864149fece85a9fcc39e
MuhammadGanny <mohammadganny@gmail.com>
2023-07-19 13:55:32 +0500

    controlling use effect behaviour

--- src/App.js
+++ src/App.js
@@ -3,34 +3,41 @@ import React, { Component, useState , useEffect} from 'react';
 
 const App = () =>{
   //state
-  const [news,setNews] = useState([])
-  const [searchQuery, setSearchQuery] = useState('react')
+  const [news,setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
   //fetch news
+
   const fetchNews = () => {
-    fetch(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+    fetch(url)
     .then(result => result.json())
     .then(data => setNews(data.hits))
     .catch(error => console.log(error))
-  }
+  };
 
   useEffect(() =>{
-    fetchNews()
-  },[searchQuery])
+    fetchNews();
+  }, [url]);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     setSearchQuery(e.target.value)
-
   }
+
+  const hundleSubmit = e => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+  }
+  
   return(
     <div>
-      <h2>News</h2>'
-      <form>
+      <h2>News</h2>
+      <form onSubmit={hundleSubmit}>
         <input type="text" value={searchQuery}  onChange={handleChange}/>
         <button>Search</button>
       </form>
       {news.map((n, i) => (<p key={i}>{n.title}</p>) )}
     </div>
-  )
+  );
 
 }
 
