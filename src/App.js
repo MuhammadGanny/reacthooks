919ababb67a02b29475eb0e9628856765aa41f5e
MuhammadGanny <mohammadganny@gmail.com>
2023-07-19 12:05:59 +0500

    counter with hook

--- src/App.js
+++ src/App.js
@@ -1,32 +1,55 @@
 
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class App extends Component {
-
-  state = {
-    count: 0
-  }
-
-  increment = () =>{
-    this.setState({
-      count: this.state.count +1
-    }
-    );
-  }
-
-
-  render(){
-    return(
-      <div>
-        <h2>Counter App</h2>
-        <button onClick={this.increment}>
-          Clicked {this.state.count} times
-         </button>
-
-      </div>
-    )
-  }
+const App = () =>{
   
-} 
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count+1)
+  };
+
+  return(
+    <div>
+      <h2>Counter App</h2>
+      <button onClick={increment}>
+          Clicked {count} times
+       </button>
+
+    </div>
+  );
+
+};
+
+
+
+
+// class App extends Component {
+
+//   state = {
+//     count: 0
+//   }
+
+//   increment = () =>{
+//     this.setState({
+//       count: this.state.count +1
+//     }
+//     );
+//   }
+
+
+//   render(){
+//     return(
+//       <div>
+//         <h2>Counter App</h2>
+//         <button onClick={this.increment}>
+//           Clicked {this.state.count} times
+//          </button>
+
+//       </div>
+//     )
+//   }
+  
+// } 
 
 export default App;
